4793afec0ea1901123162fa26f4faa63b3b03e7d
rooklift <16438795+rooklift@users.noreply.github.com>
2024-02-09 20:52:09 +0000

    Progress

--- mitm.go
+++ mitm.go
@@ -1,58 +1,59 @@
 package main
 
-// This program exists to MITM any program that is controlled by some other
-// program via stdin/out/err, for research purposes.
-
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
-const REAL_PROGRAM = "./path_to_real_program.exe"
-const LOGFILE_NAME = "mitm_log.txt"
-
-var log_chan = make(chan []byte, 128)
+var nodes_limit int
 
 func main() {
 
-	exec_command := exec.Command(REAL_PROGRAM, os.Args[1:]...)
+	var err error
+
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s [nodes] [path] [...additional args]", os.Args[0])
+		return
+	}
+
+	nodes_limit, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Invalid nodes arg: %v", err)
+		return
+	}
+
+	exec_command := exec.Command(os.Args[2], os.Args[3:]...)
 	i_pipe, _ := exec_command.StdinPipe()
 	o_pipe, _ := exec_command.StdoutPipe()
 	e_pipe, _ := exec_command.StderrPipe()
 
-	exec_command.Start()
+	err = exec_command.Start()
+	if err != nil {
+		fmt.Printf("Failed to start engine: %v", err)
+		return
+	}
 
-	go mitm(os.Stdin, i_pipe, []byte("--> "))
-	go mitm(o_pipe, os.Stdout, []byte("<-- "))
-	go mitm(e_pipe, os.Stderr, []byte("(e) "))
-	logger()
+	go mitm(o_pipe, os.Stdout)
+	go mitm(e_pipe, os.Stderr)
+	adjuster_mitm(os.Stdin, i_pipe)
 }
 
-func mitm(input io.Reader, output io.Writer, prefix []byte) {
-
+func mitm(input io.Reader, output io.Writer) {
 	scanner := bufio.NewScanner(input)
-
 	for scanner.Scan() {
-
 		output.Write(scanner.Bytes())
 		output.Write([]byte{'\n'})
-
-		log_message := make([]byte, len(prefix) + len(scanner.Bytes()))
-		copy(log_message, prefix)
-		copy(log_message[len(prefix):], scanner.Bytes())
-		log_chan <- log_message
 	}
 }
 
-func logger() {
-
-	outfile, _ := os.Create(LOGFILE_NAME)
-
-	for {
-		b := <- log_chan
-		outfile.Write(b)
-		outfile.Write([]byte{'\n'})
+func adjuster_mitm(input io.Reader, output io.Writer) {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		output.Write(scanner.Bytes())
+		output.Write([]byte{'\n'})
 	}
 }
